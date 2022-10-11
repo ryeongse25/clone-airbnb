1553092d6428626fadb49c4846316f107e9b583f
ryeongse25 <seryeong971120@gmail.com>
2022-10-11 16:18:44 +0900

    try: room slider

--- src/js/Room.js
+++ src/js/Room.js
@@ -14,12 +14,45 @@ const Room = () => {
                 img[i].style.height = img[i].clientWidth - 10 + "px";
             }
         })
+
+        let pictures = document.querySelector(".pictures");
+
+        pictures.addEventListener('mouseover', () => {
+            let btn = document.querySelectorAll(".carousel-slider button");
+
+            for (let i=0; i<btn.length; i++) {
+                btn[i].style.color = "black";
+                btn[i].style.backgroundColor = "white";
+            }
+        })
+
+        pictures.addEventListener('mouseleave', () => {
+            let btn = document.querySelectorAll(".carousel-slider button");
+
+            for (let i=0; i<btn.length; i++) {
+                btn[i].style.color = "transparent";
+                btn[i].style.backgroundColor = "transparent";
+            }
+        })
     })
 
     return (
         <div className="room">
-            <img src="./img/room1.jpeg" />
-            <div>
+            <div className="carousel-slider">
+                <div className="pictures">
+                    <img src="./img/room1.jpeg" />
+                </div>
+                <button className="prev_button"><i class="bi bi-chevron-left"></i></button>
+                <button className="next_button"><i class="bi bi-chevron-right"></i></button>
+            </div>
+            {/* <ul className="dots">
+                <li className="dot" value="0"></li>
+                <li className="dot" value="1"></li>
+                <li className="dot" value="2"></li>
+                <li className="dot" value="3"></li>
+                <li className="dot" value="4"></li>
+            </ul> */}
+            <div className="title">
                 <p>Sindun-myeon, Icheon-si, 한국</p>
                 <p>★ 4.8</p>
             </div>
